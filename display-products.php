da216ac1ea1106fc69a71712ef77a58eff8a868f
Mwan3kii <agathawanja303@gmail.com>
2025-01-13 21:38:36 +0300

    Save products to database and display them on products page

---
+++ display-products.php
@@ -0,0 +1,34 @@
+<?php
+include_once("./database.php");
+
+$sql_fetch = "SELECT * FROM products";
+$prod = new stdClass();
+$text = "";
+$result = $conn->query($sql_fetch);
+
+if ($result->num_rows > 0) {
+    // Checks if result contains rows
+    while($row = $result->fetch_assoc()) {
+    $text.= "
+            <div class='product-image'>
+                <img src='uploads/" . $row['IMAGE'] . "' alt='product' onclick='singleProduct(" . $row['ID'] . ")'>
+                <button class='cart-button'>
+                    Add to cart
+                </button>
+            </div>
+            <h4>" . $row['NAME'] . "</h4>
+            <span class='price'>
+                <del>Ksh " . $row['PRICE'] . "</del>
+                Ksh " . $row['PRICE'] . "
+            </span>
+        ";
+}
+} else {
+    echo "0 results";
+}
+
+$prod->resp = $text;
+echo $text;
+
+$conn->close();
+?>
