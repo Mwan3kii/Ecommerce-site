9d93eb581d365040908f051442c155ab3ebad846
Mwan3kii <agathawanja303@gmail.com>
2025-03-06 22:49:30 +0300

    Implement add-to-cart functionality with session storage and dynamic UI update

--- script.js
+++ script.js
@@ -50,6 +50,31 @@
 //     console.error('Products container not found!');
 // }
 
+// const productContainer = document.querySelector('.products-div');
+
+            // if (productContainer) {
+            //     products.forEach(product => {
+            //         const productDiv = document.createElement('div');
+            //         productDiv.classList.add('products-display');
+            //         productDiv.innerHTML = `
+            // <div class="product-image">
+            //                 <img src="${product.image}" alt="product" onclick="singleProduct(${product.id})">
+            //                 <button class="cart-button">
+            //                     Add to cart
+            //                 </button>
+            //             </div>
+            //             <h4>${product.name}</h4>
+            //             <span class="price">
+            //                 <del>Ksh ${product.oldPrice}</del>
+            //                 Ksh ${product.newPrice}
+            //             </span>
+            // `;
+            //         productContainer.appendChild(productDiv);
+            //     });
+            // } else {
+            //     console.error('Products container not found!');
+            // }
+
 function singleProduct(productId) {
     localStorage.setItem('selectedProductId', productId);
     window.location.href = `single-product.html?id=${productId}`;
